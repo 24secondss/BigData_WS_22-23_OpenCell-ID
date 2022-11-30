7f11b1d40d8aa821e9b39a5938e759a8536281f1
24secondss <78076663+24secondss@users.noreply.github.com>
2022-11-30 10:35:06 +0100

    Updated backend.js

--- UI/backend/backend.js
+++ UI/backend/backend.js
@@ -3,14 +3,6 @@ const app = express()
 const cors = require("cors")
 
 const mariadb = require("mariadb");
-// const pool = mariadb.createPool({
-//      host: "35.204.79.5",
-//      port: "3306", 
-//      user: "root", 
-//      password: "root",
-//      database: "ocid_cell_tower",
-//      connectionLimit: 1
-// });
 
 async function queryDatabase(statement, response) {
   let connectionToDB;
@@ -48,6 +40,7 @@ async function queryDatabase(statement, response) {
   } 
 }
 
+app.use(express.static('/backend/dist'));
 app.use(cors());
 app.get('/:longitude/:latitude/:radio', function(req, res) {
     let sqlQueryStatement = `SELECT radio, ocid_cell_tower.range FROM ocid_cell_tower WHERE radio = '${req.params.radio}' AND lon <= ('${req.params.longitude}' + 1) AND lon >= ('${req.params.longitude}' - 1) AND lat <= ('${req.params.latitude}' + 1) AND lat >= ('${req.params.latitude}' - 1) LIMIT 1;`; //WHERE st_distance_Sphere(point(lon, lat), point(${req.params.lon}, ${req.params.lat})) <= ocid_cell_tower.range * 10000 
