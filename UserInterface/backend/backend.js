b3a8810634a6404162d793cab4be01ad581de853
24secondss <78076663+24secondss@users.noreply.github.com>
2022-11-30 19:12:10 +0100

    Finalized query

--- UserInterface/backend/backend.js
+++ UserInterface/backend/backend.js
@@ -43,7 +43,7 @@ async function queryDatabase(statement, response) {
 app.use(express.static('/backend/dist'));
 app.use(cors());
 app.get('/:longitude/:latitude/:radio', function(req, res) {
-    let sqlQueryStatement = `SELECT radio, ocid_cell_tower.range FROM ocid_cell_tower WHERE radio = '${req.params.radio}' AND lon <= ('${req.params.longitude}' + 1) AND lon >= ('${req.params.longitude}' - 1) AND lat <= ('${req.params.latitude}' + 1) AND lat >= ('${req.params.latitude}' - 1) LIMIT 1;`; //WHERE st_distance_Sphere(point(lon, lat), point(${req.params.lon}, ${req.params.lat})) <= ocid_cell_tower.range * 10000 
+    let sqlQueryStatement = `SELECT radio, partition_'${req.params.radio}'.range FROM partition_'${req.params.radio}' WHERE lon <= ('${req.params.longitude}' + 1) AND lon >= ('${req.params.longitude}' - 1) AND lat <= ('${req.params.latitude}' + 1) AND lat >= ('${req.params.latitude}' - 1) LIMIT 1;`; 
     queryDatabase(sqlQueryStatement, res);
   });
 
